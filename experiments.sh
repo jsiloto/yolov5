ead84f6e6dc998375a2a0b79d9e9a3b2de6c8c37
Juliano Siloto Assine <jsilotoa@uci.edu>
2023-07-03 09:09:11 -0700

    rename

--- experiments.sh
+++ experiments.sh
@@ -5,13 +5,13 @@ PARAMETERS_HYP0="${PARAMETERS_BASE} --data bdd100k.yaml"
 PARAMETERS_HYP1="${PARAMETERS_BASE} --data bdd100kno9.yaml"
 
 
-for P in "01" "03" "06"
+for P in "02" "04" "06"
 do
     python train.py --cfg models/split/yolov5s_split.${P}.yaml ${PARAMETERS_HYP0} \
     --project hyp0 --name ${P} 2>&1 | tee -a hyp0/log${P}.txt
 done
 
-for P in "01" "03" "06"
+for P in "02" "04" "06"
 do
     python train.py --cfg models/split/yolov5s_split.${P}.yaml ${PARAMETERS_HYP1} \
     --project hyp1 --name ${P} 2>&1 | tee -a hyp1/log${P}.txt
