3bcae02bdf20fc71ee919e5b77500e0a603d3ca0
jsilotoa <jsilotoa@uci.edu>
2023-07-05 17:00:20 -0700

    Split hypotesis tests into two scripts

--- experiments_hyp0.sh
+++ experiments_hyp0.sh
@@ -2,17 +2,9 @@
 
 PARAMETERS_BASE="--img 640 --epochs 15 --batch-size 64 --weights yolov5s.pt --exist-ok"
 PARAMETERS_HYP0="${PARAMETERS_BASE} --data bdd100k.yaml"
-PARAMETERS_HYP1="${PARAMETERS_BASE} --data bdd100kno9.yaml"
 
-
-for P in "02" "04" "06"
+for P in "02" "04" "06" "10" "20"
 do
     python train.py --cfg models/split/yolov5s_split.${P}.yaml ${PARAMETERS_HYP0} \
     --project hyp0 --name ${P} 2>&1 | tee -a hyp0/log${P}.txt
-done
-
-for P in "02" "04" "06"
-do
-    python train.py --cfg models/split/yolov5s_split.${P}.yaml ${PARAMETERS_HYP1} \
-    --project hyp1 --name ${P} 2>&1 | tee -a hyp1/log${P}.txt
 done
