afd60d87ca055a4c50b76c28f65bd83450d63dd6
jsilotoa <jsilotoa@uci.edu>
2023-07-06 18:52:49 -0700

    Experiments

--- experiments_hyp1.sh
+++ experiments_hyp1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PARAMETERS_BASE="--img 640 --epochs 15 --batch-size 64 --weights yolov5s.pt --exist-ok"
+PARAMETERS_BASE="--img 640 --epochs 30 --batch-size 64 --weights yolov5s.pt --exist-ok"
 PARAMETERS_HYP1="${PARAMETERS_BASE} --data bdd100khyp1.yaml"
 
-for P in "02" "04" "06" "10" "20"
+for P in "02" "04" "06" "10"
 do
     python train.py --cfg models/split/yolov5s_split.${P}.yaml ${PARAMETERS_HYP1} \
     --project hyp1 --name ${P} 2>&1
