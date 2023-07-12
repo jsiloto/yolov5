f884c1770c3c1dd55c5ab1b800145cac0e87c2c8
jsilotoa <jsilotoa@uci.edu>
2023-07-11 21:51:35 -0700

    Eval domain

--- experiments_domain.sh
+++ experiments_domain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PARAMETERS_BASE="--img 640 --epochs 30 --batch-size 64 --exist-ok --weights yolov5s.pt --hyp data/hyps/hyp.fine-bdd.yaml"
+PARAMETERS_BASE="--img 640 --epochs 50 --batch-size 64 --exist-ok --weights yolov5s.pt --hyp data/hyps/hyp.fine-bdd.yaml"
 
 for D in "baseline" "clear" "daytime"  "night" "partly_cloudy" \
   "residential" "city_street" "dawn_dusk" "highway" "overcast" "rainy" "snowy"
